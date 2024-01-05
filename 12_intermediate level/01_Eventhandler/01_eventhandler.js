416273d619e0abb4aca01dc76e58591e24016fc4
SumukhaNidhi <109661181+sumukhanidhibs@users.noreply.github.com>
2024-01-05 18:36:57 +0000

    commit

--- 12_intermediate level/01_Eventhandler/01_eventhandler.js
+++ 12_intermediate level/01_Eventhandler/01_eventhandler.js
@@ -1,7 +1,8 @@
 const button = document.querySelector("button")
 const msg = document.querySelector(".msg")
 let clicks = 0;
-button.addEventListener('click', () => {
+button.addEventListener('click', (e) => {
     clicks = clicks + 1
     msg.innerHTML = `You clicked this button ${clicks} times`
+    e.target.innerHTML = `You clicked this button ${clicks} times`
 })
